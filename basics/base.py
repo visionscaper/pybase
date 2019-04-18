2ac2a2ff50d3709e7d4ea3a977c069fee00f5184
Freddy Snijder <freddy@visionscapers.com>
2019-04-18 18:07:38 +0200

    Allow super to have args and keyword args

--- basics/base.py
+++ basics/base.py
@@ -3,8 +3,8 @@ from basics.logging import get_logger
 
 class Base:
 
-    def __init__(self, **kwargs):
-        super().__init__(**kwargs)
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
 
         self._valid = True
         self._log = get_logger(self.__class__.__name__)
--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from distutils.core import setup
 
 setup(
     name='pybase',
-    version='0.1',
+    version='0.2',
     packages=['basics',],
     license='MIT license',
     long_description=open('README.md').read(),
