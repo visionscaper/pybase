2878f114d18764418780b678bcbf52aad557c873
Freddy Snijder <freddy@visionscapers.com>
2020-10-10 23:47:10 +0200

    Version bump

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from distutils.core import setup
 
 setup(
     name='pybase',
-    version='0.2',
+    version='0.3',
     packages=['basics',],
     license='MIT license',
     long_description=open('README.md').read(),
