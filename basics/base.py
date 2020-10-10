579b7cf68d39dca15444fcbdfb1984f0af64a36e
Freddy Snijder <freddy@visionscapers.com>
2020-10-10 23:46:31 +0200

    Made logger name a constructor parameter, added the ability to override the computation of the logger name, stringification of instances derived fromBase is now based on logger name by default

--- basics/base.py
+++ basics/base.py
@@ -3,12 +3,22 @@ from basics.logging import get_logger
 
 class Base:
 
-    def __init__(self, *args, **kwargs):
+    def __init__(self, *args, pybase_logger_name=None, **kwargs):
         super().__init__(*args, **kwargs)
 
+        if pybase_logger_name is None:
+            pybase_logger_name = self.__class__.__name__
+
+        self._pybase_logger_name = pybase_logger_name
+
         self._valid = True
-        self._log = get_logger(self.__class__.__name__)
+        self._log = get_logger(self._pybase_get_logger_name())
 
     def instance_valid(self):
         return self._valid
 
+    def _pybase_get_logger_name(self):
+        return self._pybase_logger_name
+
+    def __str__(self):
+        return self._pybase_get_logger_name()
