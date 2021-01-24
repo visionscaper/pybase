64c8c3eaa1e71f4f8c35ffe370969c97386cbde6
Freddy Snijder <freddy@visionscapers.com>
2021-01-24 23:51:15 +0100

    Renamed package

--- setup.py
+++ setup.py
@@ -2,8 +2,8 @@ import setuptools
 from distutils.core import setup
 
 setup(
-    name='pybase',
-    version='0.3.1',
+    name='visionscaper_pybase',
+    version='0.3.2',
     packages=['basics',],
     license='MIT license',
     long_description=open('README.md').read(),
